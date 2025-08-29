1bab95fb884af27d7bdd706a820cced4cef6e5c6
luongbe <backend3@the20.vn>
2025-08-29 20:44:13 +0700

    [Fix] bug

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -149,7 +149,6 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
