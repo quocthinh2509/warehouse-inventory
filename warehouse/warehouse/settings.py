e91df0c87c766835793290cf5128e1bbc09d25a5
quocthinh2509 <thinhtq2509@gmail.com>
2025-09-30 12:47:06 +0700

    🔨

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -188,9 +188,10 @@ LOGGING = {
 }
 
 
-
-
-CACHES ={
-    "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+    }
 }
 
